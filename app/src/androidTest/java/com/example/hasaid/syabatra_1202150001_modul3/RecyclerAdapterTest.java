0278052363a22e1125319e594b0c961dc2ea9939
wildanaditya <wildanaditya@365.telkomuniversity.ac.id>
2018-02-25 18:09:16 +0700

    SYABATRAHASAID_1202150001_MODUL3

---
+++ app/src/androidTest/java/com/example/hasaid/syabatra_1202150001_modul3/RecyclerAdapterTest.java
@@ -0,0 +1,18 @@
+package com.example.hasaid.syabatra_1202150001_modul3;
+
+import org.junit.Test;
+
+
+public class RecyclerAdapterTest {
+    @Test
+    public void onCreateViewHolder() throws Exception {
+    }
+
+    @Test
+    public void onBindViewHolder() throws Exception {
+    }
+
+    @Test
+    public void getItemCount() throws Exception {
+    }
+}
